b4b36be2d64c6dff964a4627c227d45aef260d5b
Maxim <sibmaks.khv@mail.ru>
2022-08-30 16:14:14 +1000

    Add Task57

---
+++ Task57/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент 
+// входных данных.
+// 1, 2, 3
+// 4, 6, 1
+// 2, 1, 6
+
+// 1 встречается 3 раза
+// 2 встречается 2 раз
+// 3 встречается 1 раз
+// 4 встречается 1 раз
+// 6 встречается 2 раза
+// В нашей исходной матрице встречаются элементы от 0 до 9
+
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FrequencyDictionary(int[,] matrix)
+{
+    int [] repeads = new int[10];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // int Num = matrix[i, j];
+            // repeads[Num] = repeads[Num] + 1;
+            repeads[matrix[i, j]]++;
+        }
+    }     
+    for (int i = 0; i < repeads.Length; i++)
+    {
+        if (repeads[i] > 0)
+        {
+            Console.WriteLine($"Количество повторений {i} - {repeads[i]} раз");
+        }
+    }
+    
+        
+}
+int[,] ourMatrix = GetMatrix(3, 4, 0, 9);
+PrintMatrix(ourMatrix);
+Console.WriteLine();
+FrequencyDictionary(ourMatrix);
