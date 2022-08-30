0c762ee9b51a89684dc3e36301e4f8002c48fd34
Maxim <sibmaks.khv@mail.ru>
2022-08-30 12:26:12 +1000

    Add Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 53: Задайте двумерный массив.
+//  Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] ourMatrix = GetMatrix(4, 4, 0, 15);
+PrintMatrix(ourMatrix);
+ReplacingStrings(ourMatrix);
+PrintMatrix(ourMatrix);
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplacingStrings(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+            int temp = matrix[0, i];
+            matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+            matrix[matrix.GetLength(0) - 1, i] = temp; 
+    }
+        Console.WriteLine();
+}
